c938803d9bc3f72a2905d09ab29143b27ca8366d
Thanos Tsianis <44327891+atsianis@users.noreply.github.com>
2020-12-19 19:27:01 +0200

    create layout component mockup

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Aux from "../../hoc/Auxiliary";
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar,Sidedrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
