cc0455b9ef01ac5024068564154496ffeb7de29d
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-20 04:01:14 +0200

    change initial state to 0 ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,10 +11,14 @@ class BurgerBuilder extends Component{
 
     state = {
         ingredients: {
-            salad:1,
-            bacon:1,
-            cheese:2,
-            meat:1
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        },
+        totalPrice:4
+    }
+
         }
     }
 
