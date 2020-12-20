54eade8189a95872a5db4ab3abd1201acd8371bd
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-20 02:51:53 +0200

    display message if burger is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     //Object.keys returns an array with the keys of the objects
     // we are tranforming an object into an array of BurgerIngredient components
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             // Array(n) returns an array of length n with empty elements
             // we create an array of length equal to the amount of the current ingredient (ingKey)
@@ -17,6 +17,9 @@ const burger = (props) => {
         .reduce((arr,el) => {
             return arr.concat(el);
         }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return(
         <div className={classes.Burger}>
             {transformedIngredients}
