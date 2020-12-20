00183ca7c706278d0825ac0ba28867d103146dd3
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-20 02:06:06 +0200

    transform props.ingredients to an array of BurgerIngredient components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //Object.keys returns an array with the keys of the objects
+    // we are tranforming an object into an array of BurgerIngredient components
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            // Array(n) returns an array of length n with empty elements
+            // we create an array of length equal to the amount of the current ingredient (ingKey)
+            // and then we map it to an array of equal length populated with the corresponding ingredients
+            return [...Array(props.ingredients[ingKey])].map((_,i)=>{
+                <BurgerIngredient key={ingKey+i} type={ingKey}/>;
+            })
+        })
     return(
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="bread-bottom"/>
+            {transformedIngredients}
         </div>
     )
 };
