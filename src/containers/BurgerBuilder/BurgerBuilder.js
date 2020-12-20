856c4ed1fc04a5b7043299ad4855c33033d6ccea
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-20 02:05:00 +0200

    create state with ingredients object

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,21 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:1
+        }
+    }
+
     render() {
         return(
             <Aux>
