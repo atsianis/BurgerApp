1c9a8ecf444ab6f18ff5cb00af02e4caf28fef7f
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-20 03:01:20 +0200

    format code

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
@@ -7,20 +6,22 @@ const burger = (props) => {
     //Object.keys returns an array with the keys of the objects
     // we are tranforming an object into an array of BurgerIngredient components
     let transformedIngredients = Object.keys(props.ingredients)
-        .map(ingKey => {
+        .map( ingKey => {
             // Array(n) returns an array of length n with empty elements
             // we create an array of length equal to the amount of the current ingredient (ingKey)
             // and then we map it to an array of equal length populated with the corresponding ingredients
-            return [...Array(props.ingredients[ingKey])].map((_,i)=>{
-                <BurgerIngredient key={ingKey+i} type={ingKey}/>;
-            })
+            return [...Array(props.ingredients[ingKey])].map((_,i)=>
+                <BurgerIngredient key={ingKey+i} type={ingKey}/>
+            );
         })
         .reduce((arr,el) => {
             return arr.concat(el);
         }, []);
+
     if (transformedIngredients.length === 0){
-        transformedIngredients = <p>Please start adding ingredients</p>
+        transformedIngredients = <p>Please start adding ingredients</p>;
     }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
