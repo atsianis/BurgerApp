87b63a066bb7ba9f1586bb35fe469d101e3e295e
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 16:18:41 +0200

    move axios post order from BurgerBuilder to ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,18 +2,57 @@ import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 
+import axios from '../../../axios-orders';
 
 class contactData extends Component {
-    state = {
-        name:'',
-        email:'',
-        adderess: {
-            street:'',
-            postalCode:''
-        }
-    }
+  state = {
+    name: "",
+    email: "",
+    adderess: {
+      street: "",
+      postalCode: "",
+    },
+    loading: false
+  };
 
-    render(){
+  orderHandler = (event) => {
+    event.preventDefault(); // because the button is in the form
+    console.log(this.props.ingredients);
+    
+    this.setState({
+      loading: true,
+    });
+
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Thanos",
+        address: {
+          street: "Test Street 24",
+          zipCode: "43243",
+          country: "Greece",
+        },
+        email: "thanos@gmail.com",
+      },
+      deliveryMethod: "fast shipping",
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        //console.log(response);
+        this.setState({
+          loading: false
+        });
+      })
+      .catch((error) => {
+        //console.log(error);
+        this.setState({
+          loading: false
+        });
+      });
+  };
 
         return(
             <div className={classes.ContactData}>
