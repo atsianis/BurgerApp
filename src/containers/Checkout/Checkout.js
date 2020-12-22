6f3613ba877c716ebaef3a7910a340a26494b2fe
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 16:14:23 +0200

    handle price inside checkout component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,13 +10,22 @@ class Checkout extends Component {
         price:0
     }
 
-    componentDidMount(){
+    componentWillMount(){
         const query = new URLSearchParams(this.props.location.search);
         const ingredients={};
+        let price = 0;
         for (let param of query.entries()){
             //['salad',1]
-            ingredients[param[0] = +param[1]];
+            if (param[0]==='price'){
+                price = param[1];
+            }else{
+                ingredients[param[0]] = +param[1];
+            }
         }
+        this.setState({
+            ingredients: ingredients, 
+            price:price
+        })
     }
 
     checkoutCancelledHandler=()=>{
