286c22050c7fe08625dd083684bc544e00362ffa
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 15:38:04 +0200

    fix path bug

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -28,7 +28,7 @@ class Checkout extends Component {
     }
 
     checkoutContinuedHandler=()=> {
-        this.props.history.replace('checkout/contact-data')
+        this.props.history.replace('/checkout/contact-data')
     }
 
     render(){
