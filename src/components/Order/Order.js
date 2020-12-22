6109bff2db84be67b5bac6dc0d40fc13ce30034c
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 20:25:00 +0200

    parse ingredients from Firebase to JS object with name and amount

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import classes from './Order.css'
 
+const order = (props) =>{
+
+    const ingredients =[];
+
+    for (let ingredientName in props.ingredients){
+        ingredients.push({name:ingredientName, amount: props.ingredients[ingredientName],})
+    }
+
+
+    return(
     </div>
-);
+    )
+};
 
 export default order;
