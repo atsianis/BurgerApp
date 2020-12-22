da3f013f5552a4886e573800938527423b426ad1
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 20:25:37 +0200

    format order's ingredients display

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -9,6 +9,21 @@ const order = (props) =>{
         ingredients.push({name:ingredientName, amount: props.ingredients[ingredientName],})
     }
 
+    const ingredientOutput = ingredients.map( ig => {
+        return (
+        <span 
+            style={
+                {textTransfrom: 'capitalize', 
+                display: 'inine-block',
+                margin: '0 8px', 
+                border: '1px solid #ccc', 
+                padding: '8px'}
+            } 
+            key={ig.name}
+        >
+                {ig.name} ({ig.amount})
+        </span>)
+    })
 
     return(
     </div>
