41bfe2971de74272a08c11fde2099479c124e5d4
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-22 14:56:03 +0200

    get the ingredients into the Checkout component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,6 +12,15 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount(){
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients={};
+        for (let param of query.entries){
+            //['salad',1]
+            ingredients[param[0] = +param[1]];
+        }
+    }
+
     checkoutCancelledHandler=()=>{
         this.props.history.goBack();
     }
