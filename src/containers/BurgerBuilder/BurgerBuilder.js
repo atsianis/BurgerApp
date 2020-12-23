2fddb04dd1b6819c9fbcde3aea2b211dc5dee1c7
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-23 18:03:44 +0200

    refactor the way the purchasability of a burger is determined

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component {
   // }
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -32,7 +31,7 @@ class BurgerBuilder extends Component {
       .catch(err => this.setState({error:true}))
   }
 
-  updatPurchasableState(ingredients) {
+  updatePurchasableState(ingredients) {
     const sum = Object.keys(ingredients)
       .map((ingkey) => {
         return ingredients[ingkey];
@@ -40,7 +39,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0 ;
   }
 
   purchaseHandler = () => {
@@ -84,7 +83,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchasableState(this.props.ings)}
             purchase={this.purchaseHandler}
           />
         </Aux>
