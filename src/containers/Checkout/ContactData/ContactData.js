d888dde868cab41d78dc78272407c87674024d52
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-23 10:45:47 +0200

    create field validation function

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,6 +83,24 @@ class contactData extends Component {
     loading: false,
   };
 
+  checkValidity(value, rules) {
+    let isValid = false;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength){
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength){
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
   orderHandler = (event) => {
     event.preventDefault(); // because the button is in the form
     
