58953a9180d9c53d639a2e41929edfb8c55da03f
Thanos Tsianis <nektariosts@gmail.com>
2020-12-24 23:25:20 +0200

    implement order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,28 @@
+import * as actiontypes from '../actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+}
+
+const reducer = (state=initialState, action) => {
+    switch (action.type){
+        case actiontypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.payload.orderData,
+                id: action.payload.orderId
+            }
+            return{
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return{
+                ...state,
+                loading: false
+            };
+        default:
+            return state;
+    }
+}
