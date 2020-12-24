922b87fef01104517d99697902b6a9941abfa42a
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-24 21:01:52 +0200

    create action creators for ingredients initialization

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,3 +1,4 @@
+import axios from "../../axios-orders";
 import * as actionTypes from './actionTypes';
 
 export const addIngredient = (ingName) => {
@@ -13,4 +14,28 @@ export const removeIngredient = (ingName) => {
         payload: ingName    }
 }
 
+export const initIngredients = () => {
+    return dispatch => {
+        axios.get('https://burgerapp-fd190-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response=> {
+        dispatch(setIngredients(response.data));
+      })
+      .catch(error=>{
+        dispatch(fetchIngredientsFailed());
+        });
+  }
+}
+  
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        payload:ingredients
+    };
+};
+
+export const fetchIngredientsFailed =() => {
+    return{
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    }
 }
