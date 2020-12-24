5fc5e9af9e074430459f0f01fd763ebd32859cbc
Thanos Tsianis <nektariosts@gmail.com>
2020-12-25 00:37:08 +0200

    dispatch purchaseInit action when we click order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as burgerBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
   // We have olny left here the state that is related to UI
@@ -42,6 +42,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinuehandler = () => {
+    this.props.onInitPurchase();
     this.props.history.push('/checkout');
   };
   
@@ -104,9 +105,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return{
-    onIngredientAdded: (ingName)=> dispatch(burgerBuilderActions.addIngredient(ingName)),
-    onIngredientRemoved: (ingName)=> dispatch(burgerBuilderActions.removeIngredient(ingName)),
-    onInit: ()=> dispatch(burgerBuilderActions.initIngredients())
+    onIngredientAdded: (ingName)=> dispatch(actions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName)=> dispatch(actions.removeIngredient(ingName)),
+    onInit: ()=> dispatch(actions.initIngredients()),
+    onInitPurchase: ()=> dispatch(actions.purchaseInit())
   }
 }
 
