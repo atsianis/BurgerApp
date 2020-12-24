58dea563ba4039700b63ccff8ebe6c4f1bf15405
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-12-24 15:25:06 +0200

    creeate burgerBuilder actions

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actionTypes';
+
+export const addIngredient = (payload) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        payload: payload
+    }
+};
+
+export const removeIngredient = (payload) => {
+    return {
+        type:actionTypes.REMOVE_INGREDIENT,
+        payload: payload    }
+}
