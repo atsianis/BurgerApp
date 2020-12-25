5e801f360e9de12aa369fd93e3419657a150da61
Thanos Tsianis <nektariosts@gmail.com>
2020-12-25 22:17:55 +0200

    implement action creators

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -46,3 +46,46 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     }
 }
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        payload: orders
+    }
+}
+
+export const fetchOrdersFail =(error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        payload: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then( res=>{
+                console.log(res);
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    console.log(key);
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch( err =>{
+                dispatch(fetchOrdersFail(err));
+            });
+    };
+
+    }
