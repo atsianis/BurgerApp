a18616b7a6349d875874b3461eb78f0250bf0780
Thanos Tsianis <nektariosts@gmail.com>
2020-12-26 01:23:00 +0200

    add link to auth page

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigationitems = () =>(
     <ul className={classes.NavigationItems}>
         <Item link="/" exact>Burger Builder</Item>
         <Item link="/orders">My Orders</Item>
+        <Item link="/auth">Authenticate</Item>
     </ul>
 );
 
