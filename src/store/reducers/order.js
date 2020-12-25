27d3cbd6984065c5d2a1bcabf72794010da1db34
Thanos Tsianis <nektariosts@gmail.com>
2020-12-25 22:18:25 +0200

    implement reducer logic

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -34,6 +34,22 @@ const reducer = (state=initialState, action) => {
                 ...state,
                 loading: false
             };
+        case actionTypes/actionTypes.FETCH_ORDERS_START:
+            return{
+                ...state,
+                loading: true
+            };
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return {
+                ...state,
+                orders: action.payload,
+                loading: false
+            };
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
         default:
             return state;
     }
