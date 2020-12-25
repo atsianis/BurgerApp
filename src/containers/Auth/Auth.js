9e01d7e8ab32c24a81ac6256bd878ceaed6c9090
Thanos Tsianis <nektariosts@gmail.com>
2020-12-25 23:58:00 +0200

    create Auth component

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+class Auth extends Component {
+  state = {
+    constrols: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Mail address",
+        },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+
+    const form = formElementsArray.map((formElement) => (
+      <Input
+        key={formElement.key}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
+    return (
+      <div>
+        <form>
+            {form}
+            <Button btn="Success">SUBMIT</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
