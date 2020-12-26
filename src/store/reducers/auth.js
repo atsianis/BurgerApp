0e6a0ded27d9b82a96c86553ef924bb30a12c4fe
Thanos Tsianis <nektariosts@gmail.com>
2020-12-26 02:28:59 +0200

    implement auth reducer

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../actions/actionTypes';
+import updateObject from '../utility';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: null
+}
+
+const reducer = (state=initialState, action) => {
+    switch(action.type){
+        case actionTypes.AUTH_START:
+            return updateObject(state, {error: null, loading: true});
+        case actionTypes.AUTH_SUCCESS:
+            return updateObject(state, {token: action.payload.token, userId: action.payload.userId, error: null, loading: false});
+        case actionTypes.AUTH_FAIL:
+            return updateObject(state, {error: action.payload.error, loading: action.payload.loading});
+        default:
+            return state;
+    }
+};
+
+export default reducer;
