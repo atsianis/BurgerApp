d70c5620fdd47e4241d1b270b070f40b59956949
Thanos Tsianis <nektariosts@gmail.com>
2020-12-26 19:18:37 +0200

    create logout action creator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,6 +21,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return{
+        type: actionTypes.AUTH_LOGOUT
+    };
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(()=>{
+            dispatch(logout());
+        }, expirationTime * 1000)
+    }
+}
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -34,6 +48,7 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
             .then(res=>{
                 dispatch(authSuccess({token: res.data.idToken, userId: res.data.localId}));
+                dispatch(checkAuthTimeout(res.data.expiresIn));
             })
             .catch(err=>{
                 dispatch(authFail(err.response.data.error));
