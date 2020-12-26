ca2175c2d109192e1d19343e752f423c94a0cc46
Thanos Tsianis <nektariosts@gmail.com>
2020-12-27 00:11:06 +0200

    hide orders link if user not authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import classes from './NavigationItems.css';
 const navigationitems = (props) =>(
     <ul className={classes.NavigationItems}>
         <Item link="/" exact>Burger Builder</Item>
-        <Item link="/orders">My Orders</Item>
+        {props.isAuth ? <Item link="/orders">My Orders</Item>: null}
         { props.isAuth ? <Item link="/logout">Logout</Item> 
             : <Item link="/auth">Authenticate</Item>}
     </ul>
