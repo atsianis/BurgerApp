3ca5b9dd23783e876a8900c2d9ed55203d99e2cd
Thanos Tsianis <nektariosts@gmail.com>
2020-12-27 00:54:22 +0200

    add boolean 'building' field to state

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -5,6 +5,7 @@ const initialState = {
   ingredients: null,
   totalPrice: 4,
   error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -27,6 +28,7 @@ const reducer = (state = initialState, action) => {
       return updateObject(state, {
         ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload],
+        building: true
       });
     case actiontypes.REMOVE_INGREDIENT:
       const updatedIng = {
@@ -36,6 +38,7 @@ const reducer = (state = initialState, action) => {
       return updateObject(state, {
         ingredients: updatedIngs,
         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.payload],
+        building: true
       });
     case actiontypes.SET_INGREDIENTS:
       return updateObject(state, {
@@ -46,6 +49,7 @@ const reducer = (state = initialState, action) => {
           meat: action.payload.meat,
         },
         totalPrice: 4,
+        building: false,
         error: false,
       });
     case actiontypes.FETCH_INGREDIENTS_FAILED:
