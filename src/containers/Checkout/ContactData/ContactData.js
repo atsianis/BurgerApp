bb1ac88ebfd54858492baa3f8c7ddb377e090477
Thanos Tsianis <nektariosts@gmail.com>
2020-12-27 04:00:05 +0200

    attach userId to order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -124,7 +124,8 @@ class contactData extends Component {
     const order = {
       ingredients: this.props.ings,
       price: this.props.price,
-      data: data
+      data: data,
+      userId: this.props.userId
     }
 
     this.props.onOrderBurger(order, this.props.token);
@@ -202,7 +203,8 @@ const mapStateToProps = state => {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 };
 
