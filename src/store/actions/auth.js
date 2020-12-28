ec5061d0788b6b11d0bbe56a667fb046b9ac11f2
Thanos Tsianis <nektariosts@gmail.com>
2020-12-28 16:12:14 +0200

    dispatch auth_initiate_logout from logout action

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,11 +22,11 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('id');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('id');
     return{
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 }
 
