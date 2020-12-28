2e84c3e5984635cd4bf6ac7aafb83d90a0d6910d
Thanos Tsianis <nektariosts@gmail.com>
2020-12-28 17:14:25 +0200

    create new watchBurger generator

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,7 @@
 import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSaga , authUserSaga} from './auth';
+import { logoutSaga, checkAuthTimeoutSaga , authUserSaga, authCheckStateSaga} from './auth';
+import { initIngredientsSaga } from './burgerBuilder';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
@@ -9,4 +10,6 @@ export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
 
+export function* watchBurger(){
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
