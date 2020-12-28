e77699ef4b425a59e4716564ad176c9cba1f7178
Thanos Tsianis <nektariosts@gmail.com>
2020-12-28 19:02:23 +0200

    move orders fetching to saga

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,88 +1,73 @@
-import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
 
 export const purchaseBurgerSuccess = (id, orderData) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_SUCCESS,
-        payload:{
-            orderId: id,
-            orderData: orderData
-        }
-        
-    };
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    payload: {
+      orderId: id,
+      orderData: orderData,
+    },
+  };
 };
 
 export const purchaseBurgerFail = (error) => {
-    return  {
-        type: actionTypes.PURCHASE_BURGER_FAIL,
-        payload: {
-            error: error
-        }
-    };
-}
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    payload: {
+      error: error,
+    },
+  };
+};
 
 export const purchaseBurgerStart = () => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_START
-    }
-}
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
+};
 
-export const purchaseBurger = (orderData,token) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER,
-        payload: {
-            token: token,
-            orderData: orderData
-        }
-    }
-}
+export const purchaseBurger = (orderData, token) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER,
+    payload: {
+      token: token,
+      orderData: orderData,
+    },
+  };
+};
 
 export const purchaseInit = () => {
-    return {
-        type: actionTypes.PURCHASE_INIT
-    }
-}
+  return {
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
 
 export const fetchOrdersSuccess = (orders) => {
-    return {
-        type: actionTypes.FETCH_ORDERS_SUCCESS,
-        payload: orders
-    }
-}
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    payload: orders,
+  };
+};
 
-export const fetchOrdersFail =(error) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_FAIL,
-        payload: error
-    }
-}
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    payload: error,
+  };
+};
 
 export const fetchOrdersStart = () => {
-    return {
-        type: actionTypes.FETCH_ORDERS_START
-    }
-}
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
 
 export const fetchOrders = (token, userId) => {
-    return dispatch => {
-        dispatch(fetchOrdersStart());
-        const queryParams = '?auth='+token+'&orderBy="userId"&equalTo="'+userId+'"';
-        axios.get('/orders.json?auth=' + queryParams)
-            .then( res=>{
-                console.log(res);
-                const fetchedOrders = [];
-                for (let key in res.data) {
-                    console.log(key);
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    })
-                }
-                dispatch(fetchOrdersSuccess(fetchedOrders));
-            })
-            .catch( err =>{
-                dispatch(fetchOrdersFail(err));
-            });
-    };
-
-    }
+  return {
+    type: actionTypes.FETCH_ORDERS,
+    payload: {
+      token: token,
+      userId: userId,
+    },
+  };
+};
--- src/store/sagas/order.js
+++ src/store/sagas/order.js
@@ -5,10 +5,33 @@ import * as actions from "../actions/index";
 export function* purchaseBurgerSaga(action) {
   yield put(actions.purchaseBurgerStart());
   try {
-    const res = yield axios.post("/orders.json?auth=" + action.payload.token, action.payload.orderData);
-    yield put(actions.purchaseBurgerSuccess(res.data.name, action.payload.orderData));
+    const res = yield axios.post(
+      "/orders.json?auth=" + action.payload.token,
+      action.payload.orderData
+    );
+    yield put(
+      actions.purchaseBurgerSuccess(res.data.name, action.payload.orderData)
+    );
   } catch (err) {
     yield put(actions.purchaseBurgerFail(err));
   }
-  
+}
+
+export function* fetchOrdersSaga(action) {
+  yield put(actions.fetchOrdersStart());
+  const queryParams = "?auth=" + action.payload.token + '&orderBy="userId"&equalTo="' + action.payload.userId + '"';
+  try {
+    const res = yield axios.get("/orders.json" + queryParams);
+    const fetchedOrders = [];
+    for (let key in res.data) {
+      console.log(key);
+      fetchedOrders.push({
+        ...res.data[key],
+        id: key,
+      });
+    }
+    yield put(actions.fetchOrdersSuccess(fetchedOrders));
+  } catch (error) {
+    yield put(actions.fetchOrdersFail(error));
+  }
 }
