50ad7c13cd5b5472674dd3a9640b76cc4a987460
Thanos Tsianis <nektariosts@gmail.com>
2020-12-28 18:24:53 +0200

    apply code formatter

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,70 +1,70 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
-export const authStart=()=> {
-    return{
-        type:actionTypes.AUTH_START
-    };
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
 };
 
 export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
+  return {
+    type: actionTypes.AUTH_SUCCESS,
     payload: {
       token: authData.token,
       userId: authData.userId
     },
-    };
+  };
 };
 
 export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        payload: error
-    };
+  return {
+    type: actionTypes.AUTH_FAIL,
+    payload: error,
+  };
 };
 
 export const logout = () => {
-    // localStorage.removeItem('token');
-    // localStorage.removeItem('expirationDate');
-    // localStorage.removeItem('id');
-    return{
-        type: actionTypes.AUTH_INITIATE_LOGOUT
-    };
-}
+  // localStorage.removeItem('token');
+  // localStorage.removeItem('expirationDate');
+  // localStorage.removeItem('id');
+  return {
+    type: actionTypes.AUTH_INITIATE_LOGOUT,
+  };
+};
 
 export const logoutSucceed = () => {
-    return {
-        type: actionTypes.AUTH_LOGOUT
-    }
-}
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-    return {
-        type: actionTypes.AUTH_CHECK_TIMEOUT,
-        payload: expirationTime
-    };
-}
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    payload: expirationTime,
+  };
+};
 
 export const auth = (email, password, isSignUp) => {
-    return{
-        type: actionTypes.AUTH_USER,
-        payload: {
-            email: email,
-            password: password,
-            isSignUp: isSignUp
-        }
-    }
-}
+  return {
+    type: actionTypes.AUTH_USER,
+    payload: {
+      email: email,
+      password: password,
+      isSignUp: isSignUp,
+    },
+  };
+};
 
 export const setAuthRedirectPath = (path) => {
-    return {
-        type: actionTypes.SET_AUTH_REDIRECT,
-        payload: path
-    };
+  return {
+    type: actionTypes.SET_AUTH_REDIRECT,
+    payload: path,
+  };
 };
 
 export const authCheckState = () => {
-    return {
-        type: actionTypes.AUTH_CHECK_STATE
-    }
+  return {
+    type: actionTypes.AUTH_CHECK_STATE,
+  };
 };
