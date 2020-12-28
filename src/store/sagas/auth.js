d9d6d60db49a32b76ebb288761ab9d799c1edbe7
Thanos Tsianis <nektariosts@gmail.com>
2020-12-28 16:51:15 +0200

    create sign in/up saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,17 +1,49 @@
-import { put } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
-import * as actions from '../actions/index';
+import { put, delay } from "redux-saga/effects";
+import axios from "axios";
+
+import * as actions from "../actions/index";
 
 export function* logoutSaga(action) {
-    yield localStorage.removeItem('token');
-    yield localStorage.removeItem('expirationDate');
-    yield localStorage.removeItem('id');
-    // same as dispatching
-    yield put(actions.logoutSucceed())
+  yield localStorage.removeItem("token");
+  yield localStorage.removeItem("expirationDate");
+  yield localStorage.removeItem("id");
+  // same as dispatching
+  yield put(actions.logoutSucceed());
 }
 
 export function* checkAuthTimeoutSaga(action) {
-    // the equivelant of setTimeout in sagas
-    yield (action.payload * 1000);
-    yield put(actions.logout());
+  // the equivelant of setTimeout in sagas
+  yield delay(action.payload * 1000);
+  yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+  const authData = {
+    email: action.payload.email,
+    password: action.payload.password,
+    returnSecureToken: true,
+  };
+  let url =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD9LDw8VjZLU8iMbHpVGU_mBfF8ORBwm04";
+  if (!action.payload.isSignUp)
+    url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD9LDw8VjZLU8iMbHpVGU_mBfF8ORBwm04";
+  // yield pauses and waits for the Promise returned to resolve and then continuous
+  try {
+    const res = yield axios.post(url, authData);
+
+    const expirationDate = new Date(
+      new Date().getTime() + res.data.expiresIn * 1000
+    );
+    localStorage.setItem("token", res.data.idToken);
+    localStorage.setItem("expirationDate", expirationDate);
+    localStorage.setItem("id", res.data.localId);
+    yield put(
+      actions.authSuccess({ token: res.data.idToken, userId: res.data.localId })
+    );
+    yield put(actions.checkAuthTimeout(res.data.expiresIn));
+  } catch (err) {
+    yield put(actions.authFail(err.response.data.error));
+  }
 }
